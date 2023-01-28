9dd48d5a7e35b01bc3c99b40954217c0075d9aa6
eddiereilly01 <47450324+eddiereilly01@users.noreply.github.com>
2023-01-28 01:02:39 +0000

    proper module folder structure for easy install

---
+++ mutationfunclib/README.md
@@ -0,0 +1,2 @@
+#This is a simple module containing my implementation of
+some mutation functions useful for genetic algorithms
---
+++ mutationfunclib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mutationfunclib',
+    version='0.1',
+    description='A small library I made to have mutation functions handy when I need them',
+    author='Edward O Reilly',
+    author_email='edwardjoreilly01@gmail.com',
+    packages=['mutationfunclib'],  #same as the directory name
+    install_requires=[] #list any dependencies your library needs
+)
